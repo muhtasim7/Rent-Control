4e88d8748d913536b7c176cf11007a72f7741b12
Muhtasim Khan <70777174+muhtasim7@users.noreply.github.com>
2024-06-20 08:21:25 -0400

    Add files via upload

---
+++ cloud_env.bat
@@ -0,0 +1,7 @@
+ECHO OFF
+CLS
+SET PATH=C:\Users\muhta\OneDrive\Desktop\python coding\Rent keep up\google-cloud-sdk\bin;%PATH%;
+cd C:\Users\muhta\OneDrive\Desktop\python coding\Rent keep up
+ECHO Welcome to the Google Cloud CLI! Run "gcloud -h" to get the list of available commands.
+ECHO ---
+ECHO ON
